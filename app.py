e2bcfa8aa0ab12e8bea3c9a085e1c07473bb340e
AskariSyed <152210170+AskariSyed@users.noreply.github.com>
2026-04-21 11:03:58 +0500

    For Pull Request

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello From Hassan Askari - DevOps Engineer"
+    return "Hello From Hassan Askari - DevOps Engineer For Lab Mid"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
